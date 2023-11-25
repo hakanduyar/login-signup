1b18d311e65d2fdaf2af36e0d8a19a1344bfcc43
rikords <iamhakanduyar@gmail.com>
2023-11-25 22:16:40 +0300

    The diff shows changes in the LoginSignup component.
    In the CSS file, the filter property of the .container::after element is changed from blur(10px) to blur(10px);.
    
    In the JSX file, the state variable action is initialized to "Login" instead of "Sign Up".
    
    Additionally, an input field for the user's name is added when the action state is set to "Sign Up".
    
    The forgot-password div is only rendered when the action state is set to "Login".
    
    The submit button now changes text based on the action state.

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -6,7 +6,7 @@ import email_icon from "../Assets/email.png";
 import password_icon from "../Assets/password.png";
 
 const LoginSignup = () => {
-  const [action, setAction] = useState("Sign Up");
+  const [action, setAction] = useState("Login");
   return (
     <div className="container">
       <div className="header">
@@ -14,10 +14,15 @@ const LoginSignup = () => {
         <div className="underline"></div>
       </div>
       <div className="inputs">
-        <div className="input">
-          <img src={user_icon} alt="" />
-          <input type="text" placeholder="name" />
-        </div>
+        {action === "Login" ? (
+          <div></div>
+        ) : (
+          <div className="input">
+            <img src={user_icon} alt="" />
+            <input type="text" placeholder="name" />
+          </div>
+        )}
+
         <div className="input">
           <img src={email_icon} alt="" />
           <input type="email" placeholder="Email Id" />
@@ -27,12 +32,31 @@ const LoginSignup = () => {
           <input type="password" placeholder="Password" />
         </div>
       </div>
-      <div className="forgot-password">
-        Lost Password? <span>Click Here</span>
-      </div>
+      {action === "Sign Up" ? (
+        <div></div>
+      ) : (
+        <div className="forgot-password">
+          Lost Password? <span>Click Here</span>
+        </div>
+      )}
+
       <div className="submit-container">
-        <div className="submit">Sign Up</div>
-        <div className="submit">Login</div>
+        <div
+          className={action === "Login" ? "submit gray" : "submit"}
+          onClick={() => {
+            setAction("Sign Up");
+          }}
+        >
+          Sign Up
+        </div>
+        <div
+          className={action === "Sign Up" ? "submit gray" : "submit"}
+          onClick={() => {
+            setAction("Login");
+          }}
+        >
+          Login
+        </div>
       </div>
     </div>
   );
